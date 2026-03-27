2008e1c3cabc2307e26420f9ebda57b3d4ef626b
AnierinB <anierin@evolution-x.org>
2026-03-26 21:34:00 -0700

    AynParts: Initial gamepad settings
    Layout (nintendo/xbox)
    Triggers (analog/digital)
    M1/M2 button mapping
    
    Change-Id: I546c913a8d982521526060c84b564b7b139d9dea
    Co-authored-by: Bal?zs Triszka <info@balika011.hu>
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- sepolicy/qti/SEPolicy.mk
+++ sepolicy/qti/SEPolicy.mk
@@ -3,5 +3,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    hardware/ayn/sepolicy/qti/vendor
+
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     hardware/ayn/sepolicy/qti/private
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    hardware/ayn/sepolicy/qti/public
