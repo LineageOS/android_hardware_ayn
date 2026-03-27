6c83185cc5c20d8fee448a41300708b99aaf9dfc
AnierinB <anierin@evolution-x.org>
2026-03-26 21:34:00 -0700

    AynParts: Initial gamepad settings
    Layout (nintendo/xbox)
    Triggers (analog/digital)
    M1/M2 button mapping
    
    Change-Id: I546c913a8d982521526060c84b564b7b139d9dea
    Co-authored-by: Bal?zs Triszka <info@balika011.hu>
    Signed-off-by: AnierinB <anierin@evolution-x.org>

---
+++ packages/AynParts/configs/idc/Vendor_057e_Product_2009_Version_1234.idc
@@ -0,0 +1,10 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Moorechip emulated Nintendo Switch Pro controller
+
+device.internal=1
+
+keyboard.layout = Vendor_057e_Product_2009_Version_1234
---
+++ packages/AynParts/src/org/lineageos/settings/ayn/BootCompletedReceiver.kt
@@ -0,0 +1,37 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings.ayn
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.preference.PreferenceManager
+
+import org.lineageos.settings.ayn.utils.NodeUtils
+
+class BootCompletedReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context)
+        val allPrefs = sharedPrefs.all
+
+        allPrefs.forEach { (key, value) ->
+            if (key.endsWith(":node")) {
+                val nodePath = value as? String
+                val prefKey = key.removeSuffix(":node")
+                val prefValue = allPrefs[prefKey] as? String
+
+                if (nodePath != null && prefValue != null && NodeUtils.exists(nodePath)) {
+                    NodeUtils.write(nodePath, prefValue)
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "BootCompletedReceiver"
+    }
+}
