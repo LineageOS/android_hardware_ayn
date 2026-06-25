06b8c6461717b29f0392a32382e686c116dd8802
AnierinB <anierin@evolution-x.org>
2026-06-25 22:58:04 +0000

    ayn: packages: AynParts: Add RGB lighting
    Change-Id: I2b585f38045d6c6b2d873a42cb694d80cf5ca9d4
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- packages/AynParts/src/org/lineageos/settings/ayn/AynSettingsFragment.kt
+++ packages/AynParts/src/org/lineageos/settings/ayn/AynSettingsFragment.kt
@@ -7,16 +7,28 @@ package org.lineageos.settings.ayn
 
 import android.os.Bundle
 import androidx.preference.Preference
+import androidx.preference.PreferenceCategory
 import com.android.settingslib.widget.SettingsBasePreferenceFragment
+import org.lineageos.settings.ayn.utils.LightUtils
 
 class AynSettingsFragment :
     SettingsBasePreferenceFragment(), Preference.OnPreferenceChangeListener {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.ayn_panel, rootKey)
+
+        if (!LightUtils.supportsRGB) {
+            findPreference<PreferenceCategory>(KEY_GAMEPAD_LIGHTS_CATEGORY)?.let {
+                preferenceScreen.removePreference(it)
+            }
+        }
     }
 
     override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
         return true
+    }
+
+    companion object {
+        private const val KEY_GAMEPAD_LIGHTS_CATEGORY = "gamepad_lights_category"
     }
 }
