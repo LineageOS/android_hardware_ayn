8e7e7ae6e52e06c1d7638a60db3bc86b6d2f7556
AnierinB <anierin@evolution-x.org>
2026-07-01 01:02:33 +0000

    ayn: Import common lineage overlays
    Change-Id: I82f030c7698550ac87a137d7cb518d404558d049
    Signed-off-by: AnierinB <anierin@evolution-x.org>

---
+++ overlay/lineage/lineage.mk
@@ -0,0 +1,10 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_PACKAGES += \
+    LineageSDKOverlayCommon
+
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
