4874b75942ea874dea5aa01483aea2939c2420ec
AnierinB <anierin@evolution-x.org>
2026-08-11 00:59:00 +0000

    ayn: services: Add charger_rgb
    Sets the color of RGB lights based on battery level in charge-only mode.
    Since we've wired the Qualcomm panel notifier up in the RGB drivers to
    receive callbacks, the LEDs automatically turn off during panel BLANK
    and on during panel UNBLANK.
    
    Color thresholds:
    - Green: 90% & above
    - Yellow: 50% to 89%
    - Orange: 25% to 49%
    - Red: Below 25%
    
    Change-Id: I30952b0264981ba493245f6c197e9e29077b4a43
    Signed-off-by: AnierinB <anierin@evolution-x.org>

---
+++ services/charger_rgb/main.cpp
@@ -0,0 +1,58 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <android-base/file.h>
+#include <unistd.h>
+#include <string>
+#include <vector>
+
+int main() {
+    const std::string RED = "255 0 0";
+    const std::string ORANGE = "255 25 0";
+    const std::string YELLOW = "255 255 0";
+    const std::string GREEN = "0 255 0";
+    const std::string BRIGHTNESS = "127";
+    const std::string CAPACITY = "/sys/class/power_supply/battery/capacity";
+
+    const std::vector<std::string> sides = {"left", "right"};
+
+    const std::vector<std::string> leds = {"stick",   "stick:0", "stick:1",
+                                           "stick:2", "stick:3", "strip"};
+
+    while (true) {
+        int battery_level = 50;
+        std::string capacity;
+
+        if (android::base::ReadFileToString(CAPACITY, &capacity)) {
+            battery_level = std::stoi(capacity);
+        }
+
+        std::string color;
+        if (battery_level >= 90) {
+            color = GREEN;
+        } else if (battery_level >= 50) {
+            color = YELLOW;
+        } else if (battery_level >= 25) {
+            color = ORANGE;
+        } else {
+            color = RED;
+        }
+
+        for (const std::string& side : sides) {
+            for (const std::string& led : leds) {
+                std::string path = "/sys/class/leds/" + side + ":" + led;
+
+                if (!access(path.c_str(), F_OK)) {
+                    android::base::WriteStringToFile(BRIGHTNESS, path + "/brightness");
+                    android::base::WriteStringToFile(color, path + "/multi_intensity");
+                }
+            }
+        }
+
+        sleep(120);
+    }
+
+    return 0;
+}
