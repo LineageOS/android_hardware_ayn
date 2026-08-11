650ce065bd19f044fc2055185268d1727a8b04ec
AnierinB <anierin@evolution-x.org>
2026-08-11 10:19:45 +0000

    ayn: packages: AynParts: Run pre-commit
    Change-Id: I98af477f9c96b9d12d183c72adaf1625f41a6072
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- packages/AynParts/src/org/lineageos/settings/ayn/preferences/NodeListPreference.kt
+++ packages/AynParts/src/org/lineageos/settings/ayn/preferences/NodeListPreference.kt
@@ -9,15 +9,16 @@ import android.content.Context
 import android.util.AttributeSet
 import androidx.core.content.withStyledAttributes
 import androidx.preference.ListPreference
-
 import org.lineageos.settings.ayn.R
 import org.lineageos.settings.ayn.utils.NodeUtils
 
-class NodeListPreference @JvmOverloads constructor(
+class NodeListPreference
+@JvmOverloads
+constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = androidx.preference.R.attr.dialogPreferenceStyle,
-    defStyleRes: Int = 0
+    defStyleRes: Int = 0,
 ) : ListPreference(context, attrs, defStyleAttr, defStyleRes) {
 
     private var nodePath: String? = null
@@ -27,7 +28,12 @@ class NodeListPreference @JvmOverloads constructor(
     private val originalValues = entryValues
 
     init {
-        context.withStyledAttributes(attrs, R.styleable.NodeListPreference, defStyleAttr, defStyleRes) {
+        context.withStyledAttributes(
+            attrs,
+            R.styleable.NodeListPreference,
+            defStyleAttr,
+            defStyleRes,
+        ) {
             nodePath = getString(R.styleable.NodeListPreference_node)
             siblingKey = getString(R.styleable.NodeListPreference_sibling)
         }
@@ -53,9 +59,11 @@ class NodeListPreference @JvmOverloads constructor(
             val defaultValue = entryValues[0].toString()
 
             if (siblingValue != defaultValue) {
-                val (validEntries, validValues) = entries.zip(entryValues)
-                    .filter { (_, value) -> value.toString() != siblingValue }
-                    .unzip()
+                val (validEntries, validValues) =
+                    entries
+                        .zip(entryValues)
+                        .filter { (_, value) -> value.toString() != siblingValue }
+                        .unzip()
 
                 entries = validEntries.toTypedArray()
                 entryValues = validValues.toTypedArray()
@@ -87,8 +95,8 @@ class NodeListPreference @JvmOverloads constructor(
     }
 
     private fun getValueFromNode(): String? {
-        return nodePath?.takeIf { NodeUtils.exists(it) }?.let { path ->
-            NodeUtils.read(path)?.takeIf { entryValues.contains(it) }
-        }
+        return nodePath
+            ?.takeIf { NodeUtils.exists(it) }
+            ?.let { path -> NodeUtils.read(path)?.takeIf { entryValues.contains(it) } }
     }
 }
