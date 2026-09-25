09da3d2724050248cb437bda0cf8f068ba7f4e20
AnierinB <anierin@evolution-x.org>
2026-09-25 00:14:45 +0000

    AynParts: Pass a custom fragment title to GameControllerFragment
    We spoof our built-in controller's input device name as "Xbox Wireless
    Controller" to ensure compatibility with games that check input device
    names. Pass a more accurate input device name to ControllerUtils so that
    GameControllerFragment reflects the actual hardware instead of the
    spoofed input device name string from the driver.
    
    Change-Id: I44c988a0cb5340952ed0f621045e605267dde662
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- packages/AynParts/src/org/lineageos/settings/ayn/AynSettingsFragment.kt
+++ packages/AynParts/src/org/lineageos/settings/ayn/AynSettingsFragment.kt
@@ -19,7 +19,11 @@ class AynSettingsFragment :
         setPreferencesFromResource(R.xml.ayn_panel, rootKey)
 
         findPreference<Preference>(KEY_GAMEPAD_REMAPPING)?.setOnPreferenceClickListener {
-            ControllerUtils.launchControllerRemapping(requireContext(), CONTROLLER_DESCRIPTOR)
+            ControllerUtils.launchControllerRemapping(
+                requireContext(),
+                CONTROLLER_DESCRIPTOR,
+                getString(R.string.moorechip_controller),
+            )
             true
         }
 
